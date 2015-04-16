da42daf4385215993b9d0b2d49610c96f977aca7
Gabriel Baravdish <gabrielbaravdish@gmail.com>
2015-04-16 19:41:22 +0200

    qq

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,13 @@
+// 
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "This line has no purpose, but it has to be here." << endl;
+	return 0;
+}
+
