ea93bbebc6beb02a027d75b1b92c12e97b6dde71
Gabriel Baravdish <gabrielbaravdish@gmail.com>
2015-04-18 12:08:05 +0200

    Testing plugin4...

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,13 +1,15 @@
-// 
-
 #include "stdafx.h"
 #include <iostream>
+#include "opencv2/highgui/highgui.hpp"
 
-using namespace std;
+using namespace cv;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	cout << "This line has no purpose, but it has to be here." << endl;
+	Mat M(7, 7, CV_32FC2, Scalar(1,3));
+	std::cout << "Testing plugin4" << endl;
+	std::cout << "This line has no purpose, but it has to be here." << std::endl;
+
 	return 0;
 }
 
