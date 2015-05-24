209b5ce25c91668b0141b0e84de903d2a016e48f
Gabriel Baravdish <gabrielbaravdish@gmail.com>
2015-05-24 18:52:39 +0200

    Added image database loader(the constructor) in DB.cpp - Added a library header dirent.h to read files in folders. - Added tracking and error messages for invalid image read. - Added some print-information about the database. - Make sure you have the correct paths to mainDirectory and folders.

--- include/DB.hpp
+++ include/DB.hpp
@@ -1,16 +1,33 @@
 #ifndef DB_H
 #define DB_H
 #include <vector>
+#include <iostream>
+#include <opencv2/opencv.hpp>
 #include "Image.hpp"
+#include "dirent.h"
+
+// Visual studio complains about collision with std and cv...
+//using namespace std;
+
+// A simple solution
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+
+using namespace cv;
 
 class DB
 {
 	private:
-	 	vector<Image> Images;
+	 	vector<Image> images;
 	public:
-	    DB();
+	    DB(string directory, vector<string> folders);
 	    void loadImages();
 	    void saveImages();
+		void printInvalidImageInformation(vector<int> invalidImages, string folder, int nImages);
+		vector<Mat> image_vector;
 };
  
 #endif
--- src/Main.cpp
+++ src/Main.cpp
@@ -5,11 +5,25 @@
 #include "Image.hpp"
 #include "CorrelationMatrix.hpp"
 
-using namespace std;
-using namespace cv;
 
 int main()
 {
-	cout << "This is main!" << endl;
+	// Setup directory and folder paths, no recursive solution yet
+	string mainDirectory = "C:/Users/Gabriel/Desktop/Bildatabaser/Bilddatabaser_TNM025/dataset/";
+	vector<string> inputFolders = { "animal2", "beach2", "cat2", "colorful2",
+								      "doll2", "elegant2", "flower2", "food2", "formal2", "garden2" };
+
+	// Initilize the database
+	DB database = DB(mainDirectory, inputFolders);
+	
+	// Step through some images in the database
+	cv::namedWindow("Window", 1);
+	for (auto& p : database.image_vector)
+	{
+		imshow("Window", p);
+		cvWaitKey(10000);
+	}
+
+	system("pause");
 	return 0;
 }
