1c85527a3b97b138bf5db3017434e17f68e246cb
Gabriel Baravdish <gabrielbaravdish@gmail.com>
2015-05-25 09:22:16 +0200

    Removed imshow of database

--- include/DB.hpp
+++ include/DB.hpp
@@ -6,28 +6,29 @@
 #include "Image.hpp"
 #include "dirent.h"
 
-// Visual studio complains about collision with std and cv...
 //using namespace std;
 
-// A simple solution
+// Visual studio complains about collision with std and cv.
+// A simple solution...
 using std::cout;
 using std::cin;
 using std::endl;
 using std::vector;
 using std::string;
-
+using std::map;
 using namespace cv;
 
 class DB
 {
 	private:
 	 	vector<Image> images;
+		vector<Mat> image_vector;
 	public:
 	    DB(string directory, vector<string> folders);
 	    void loadImages();
 	    void saveImages();
 		void printInvalidImageInformation(vector<int> invalidImages, string folder, int nImages);
-		vector<Mat> image_vector;
+		vector<Mat> getImageDatabase();
 };
  
 #endif
--- src/Main.cpp
+++ src/Main.cpp
@@ -11,19 +11,11 @@ int main()
 	// Setup directory and folder paths, no recursive solution yet
 	string mainDirectory = "C:/Users/Gabriel/Desktop/Bildatabaser/Bilddatabaser_TNM025/dataset/";
 	vector<string> inputFolders = { "animal2", "beach2", "cat2", "colorful2",
-								      "doll2", "elegant2", "flower2", "food2", "formal2", "garden2" };
+								    "doll2", "elegant2", "flower2", "food2", "formal2", "garden2" };
 
 	// Initilize the database
 	DB database = DB(mainDirectory, inputFolders);
 	
-	// Step through some images in the database
-	cv::namedWindow("Window", 1);
-	for (auto& p : database.image_vector)
-	{
-		imshow("Window", p);
-		cvWaitKey(10000);
-	}
-
 	system("pause");
 	return 0;
 }
